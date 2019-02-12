affc898a76e5bfbd687ea08d8125ea08b1e1e304
Michael Krimm <Michael.Krimm@ZIRMED.com>
2019-02-12 17:17:12 -0500

    continue set up of project and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { MaterialModule } from './material.module';
-import { ProductPageComponent } from './my-nav/product-page/product-page.component';
+import { AppRoutingModule } from './routing/app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyNavComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent, MyNavComponent, ProductPageComponent]
+  bootstrap: [AppComponent, MyNavComponent, ProductPageComponent, HomeComponent]
 })
 export class AppModule { }
---
+++ src/app/routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { ProductPageComponent } from '../product-page/product-page.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent, pathMatch: 'full'},
+  {path: 'products', component: ProductPageComponent, pathMatch: 'full'},
+  {path: 'about', component: HomeComponent, pathMatch: 'full' }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
