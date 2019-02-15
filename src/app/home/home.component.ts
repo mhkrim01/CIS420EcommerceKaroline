f7afcacdb4f0c8ad45475527c47b84901894405b
Michael Krimm <Michael.Krimm@ZIRMED.com>
2019-02-15 18:13:13 -0500

    setup animations for social media links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, animate, transition, style } from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('showSocialMedia', [
+      state('true', style({ maxHeight: '40vh' })),
+      state('false', style({ maxHeight: 0, display: 'none' })),
+      transition('false => true', [style({ opacity: '0' }), animate('400ms 300ms')]),
+      transition('true => false', animate('400ms'))
+  ])]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public flyIn
+
+  constructor() {
+    this.flyIn = false;
+   }
 
   ngOnInit() {
   }
 
+  toggleFlyIn(){
+    this.flyIn = !this.flyIn;
+  }
 }
