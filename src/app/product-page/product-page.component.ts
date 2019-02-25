f673989a657163b62f3fe832d3fef61fd7828266
Michael Krimm <Michael.Krimm@ZIRMED.com>
2019-02-24 20:30:10 -0500

    keep formatting things, get about page looking decentish

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
@@ -10,11 +11,15 @@ export class ProductPageComponent implements OnInit {
   testProd = {name: 'test'};
   products: Array<any> = [this.testProd, this.testProd, this.testProd, this.testProd, this.testProd, this.testProd]
 
-  constructor() {
+  constructor(private _router: Router) {
     console.log(this.products.length);
    }
 
   ngOnInit() {
+  }
+
+  navigateToProducts(){
+    this._router.navigateByUrl('/products');
   }
 
 }
--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { ProductPageComponent } from '../product-page/product-page.component';
+import { AboutComponent } from '../about/about.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent, pathMatch: 'full'},
   {path: 'products', component: ProductPageComponent, pathMatch: 'full'},
-  {path: 'about', component: HomeComponent, pathMatch: 'full' }
+  {path: 'about', component: AboutComponent, pathMatch: 'full' }
 ]
 
 @NgModule({
